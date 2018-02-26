8611e88f890e2c067b6b453606e058e517e50485
Cameron Jenkinson <camjenkinson@gmail.com>
2018-02-26 19:42:52 +0000

    Add Loader component

---
+++ src/components/Loader/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	loader: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingHorizontal: 4,
+		paddingVertical: 8,
+		backgroundColor: '#fff',
+		borderTopColor: '#D9E0E6',
+		borderTopWidth: 1
+	}
+});
