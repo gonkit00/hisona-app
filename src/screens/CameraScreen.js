214e7a1fde5fe50d696a90e813ea490af3d68408
Cameron Jenkinson <camjenkinson@gmail.com>
2018-02-28 19:12:48 +0100

    Add CameraScreen

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,63 +1,86 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { Camera, Permissions } from 'expo';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Camera, FileSystem, Permissions } from 'expo';
+
+import { Actions } from 'react-native-router-flux';
 
 class CameraView extends Component {
-	state = {
-		hasCameraPermission: null,
-		type: Camera.Constants.Type.back
-	};
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			hasCameraPermission: null,
+			type: Camera.Constants.Type.back
+		};
+	}
 
 	async componentWillMount() {
 		const { status } = await Permissions.askAsync(Permissions.CAMERA);
 		this.setState({ hasCameraPermission: status === 'granted' });
 	}
 
+	componentDidMount() {
+		FileSystem.makeDirectoryAsync(
+			`${FileSystem.documentDirectory} photos`
+		).catch(e => {
+			console.log(e, 'Directory exists');
+		});
+	}
+
+	takePicture = async function() {
+		if (this.camera) {
+			this.camera.takePictureAsync().then(photoData => {
+				// dispatch, recognise_image_request
+				Actions.push('recogniseScreen', { photoData });
+			});
+		}
+	};
+
 	render() {
 		const { hasCameraPermission } = this.state;
 		if (hasCameraPermission === null) {
 			return <View />;
 		} else if (hasCameraPermission === false) {
-			return <Text>No access to camera</Text>;
+			return <Text>Hisona can't access your camera :(</Text>;
 		}
-			return (
-				<View style={{ flex: 1 }}>
-					<Camera style={{ flex: 1 }} type={this.state.type}>
-						<View
-							style={{
-								flex: 1,
-								backgroundColor: 'transparent',
-								flexDirection: 'row'
-							}}
-						>
-							<TouchableOpacity
-								style={{
-									flex: 0.1,
-									alignSelf: 'flex-end',
-									alignItems: 'center'
-								}}
-								onPress={() => {
-									this.setState({
-										type:
-											this.state.type === Camera.Constants.Type.back
-												? Camera.Constants.Type.front
-												: Camera.Constants.Type.back
-									});
-								}}
-							>
-								<Text
-									style={{ fontSize: 18, marginBottom: 10, color: 'white' }}
-								>
-									{' '}
-									Flip{' '}
-								</Text>
-							</TouchableOpacity>
-						</View>
-					</Camera>
-				</View>
-			);
-
+		return (
+			<View style={{ flex: 1 }}>
+				<Camera
+					style={{ flex: 1 }}
+					type={this.state.type}
+					ref={ref => {
+						this.camera = ref;
+					}}
+				>
+					<View style={styles.actionsContainer}>
+						<TouchableOpacity
+							style={styles.snapButton}
+							onPress={this.takePicture.bind(this)}
+						/>
+					</View>
+				</Camera>
+			</View>
+		);
 	}
 }
+
+const styles = StyleSheet.create({
+	actionsContainer: {
+		flex: 1,
+		backgroundColor: 'transparent',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		marginBottom: 24
+	},
+	snapButton: {
+		height: 80,
+		width: 80,
+		backgroundColor: 'rgba(0, 0, 0, 0.3)',
+		borderRadius: 40,
+		borderColor: 'white',
+		borderWidth: 3,
+		alignSelf: 'flex-end'
+	}
+});
 
 export default CameraView;
