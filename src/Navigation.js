2dd51a93bf92ed12777e21dab6c9814d92b7221e
Cameron Jenkinson <camjenkinson@gmail.com>
2018-03-04 17:52:44 +0100

    Add buttons to right / left render to root scene

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Text, Image, TouchableOpacity } from 'react-native';
 import { Scene, Router, Modal, Actions } from 'react-native-router-flux';
 
-import { ScreenHeader } from '~/components/ScreenHeader';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { Header } from '~/components/Header';
 import ChatsListScreen from '~/screens/ChatsListScreen';
 import ChatScreen from '~/screens/ChatScreen';
 import CameraScreen from '~/screens/CameraScreen';
 import RecogniseScreen from '~/screens/RecogniseScreen';
 import MatchScreen from '~/screens/MatchScreen';
+
 
 const Navigation = () => (
 	<Router>
@@ -15,23 +19,24 @@ const Navigation = () => (
 				<Scene
 					key="chatsListScreen"
 					component={ChatsListScreen}
-					title={'HISONA'}
+					title="HISONA"
+					titleStyle={{ fontFamily: 'barlow-bold', fontSize: 20 }}
+					renderRightButton={() =>
+            <TouchableOpacity onPress={() => Actions.cameraScreen()}>
+              <MaterialIcons name="add" size={32} />
+            </TouchableOpacity>
+          }
 					initial
 				/>
-				<Scene key="chatScreen" component={ChatScreen} />
+				<Scene
+					key="chatScreen"
+					component={ChatScreen}
+					titleStyle={{ fontFamily: 'barlow-medium', fontSize: 20 }}
+				/>
 			</Scene>
-      <Scene
-        key="cameraScreen"
-        component={CameraScreen}
-        back
-        navTransparent
-      />
-      <Scene
-        key="recogniseScreen"
-        component={RecogniseScreen}
-        navTransparent
-      />
-      <Scene key="matchScreen" component={MatchScreen} navTransparent />
+			<Scene key="cameraScreen" component={CameraScreen} back navTransparent />
+			<Scene key="recogniseScreen" component={RecogniseScreen} navTransparent />
+			<Scene key="matchScreen" component={MatchScreen} navTransparent />
 		</Modal>
 	</Router>
 );
